001f55c30695cae8f0f74e96022097ae8cabd7f3
dacharat <dacharat2013@gmail.com>
2018-12-26 22:49:55 +0700

    :pig: can delete comment

--- src/Comment.js
+++ src/Comment.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
 
 class Comment extends React.Component {
+  handleDelete = () => {    
+    this.props.dispatch({ type: "DELETE_COMMENT", id: this.props.comment.id });
+  };
+
   render() {
     return (
       <div>
         <h2>{this.props.comment.name}</h2>
         <p>{this.props.comment.message}</p>
+        <button>Edit</button>
+        <button onClick={this.handleDelete}>Delete</button>
       </div>
     );
   }
 }
 
-export default Comment;
+export default connect()(Comment);
--- src/commentReducer.js
+++ src/commentReducer.js
@@ -1,8 +1,11 @@
+import { comment } from "postcss";
+
 export default (state = [], action) => {
   switch (action.type) {
     case "ADD_COMMENT":
       return state.concat([action.data]);
-
+    case 'DELETE_COMMENT':
+      return state.filter(comment => comment.id !== action.id)
     default:
       return state;
   }
